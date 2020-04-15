7ea0454a2c0adaf52e8d6f40c9c046d9616043a9
@Inna <55448200+Inna-WD@users.noreply.github.com>
2020-04-15 19:42:03 -0400

    1504

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './src/components/Header/Header';
+import Messenger from './src/components/Messenger/Messenger';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Messenger />
+    </div>
+  );
+}
+
+export default App;
--- index.md
+++
@@ -1,1 +0,0 @@
-This is a Communication App site
